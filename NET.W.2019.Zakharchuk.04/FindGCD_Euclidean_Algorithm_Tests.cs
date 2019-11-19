c21919bd53886c6e89e74017b88b61ebf7907957
Andrei Zakharchuk <andraging@gmail.com>
2019-11-20 00:05:58 +0300

    Task1
    Add solution of first task

---
+++ NET.W.2019.Zakharchuk.04/FindGCD_Euclidean_Algorithm_Tests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FindGCD.Tests
+{
+    [TestClass]
+    public class FindGCD_Euclidean_Algorithm_Tests
+    {
+        [TestMethod]
+        public void GetGcdTestFromIntegers_ArgumentNullException()
+        {   
+            Assert.ThrowsException<ArgumentNullException>(() => GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, null));
+        }
+
+        [TestMethod]
+        public void GetGcdTestFromIntegers_minus5_5_55()
+        {            
+            Assert.AreEqual(GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, -5, 5, -55), 5);
+            Assert.IsTrue(TestGetGcdFromIntegers_TimeSpend(timeSpend));
+        }
+
+        [TestMethod]
+        public void GetGcdTestFromIntegers_1_0_0_0()
+        {
+            Assert.AreEqual(GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, 1, 0, 0, 0), 1);
+            Assert.IsTrue(TestGetGcdFromIntegers_TimeSpend(timeSpend));
+        }
+
+        [TestMethod]
+        public void GetGcdTestFromIntegers_0_1_0_1()
+        {
+            Assert.AreEqual(GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, 0, 1, 0, 1), 1);
+            Assert.IsTrue(TestGetGcdFromIntegers_TimeSpend(timeSpend));
+        }
+
+        [TestMethod]
+        public void GetGcdTestFromIntegers_intMinValue_0_0()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend,  int.MinValue, 0, 0));            
+        }
+
+        [TestMethod]
+        public void GetGcdTestFromIntegers_0_0_0()
+        {
+            Assert.AreEqual(GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, 0, 0, 0), 0);
+            Assert.IsTrue(TestGetGcdFromIntegers_TimeSpend(timeSpend));            
+        }
+
+        [TestMethod]
+        public void GetGcdTestFromIntegers_0_100_0()
+        {
+            Assert.AreEqual(GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, 0, 100, 0), 100);
+            Assert.IsTrue(TestGetGcdFromIntegers_TimeSpend(timeSpend));            
+        }
+
+        [TestMethod]
+        public void GetGcdTestFromIntegers_5_minus7455_1075()
+        {
+            Assert.AreEqual(GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, 5, -7455, 1075), 5);
+            Assert.IsTrue(TestGetGcdFromIntegers_TimeSpend(timeSpend));            
+        }
+
+        [TestMethod]
+        public void GetGcdTestFromIntegers_9_54_99()
+        {
+            Assert.AreEqual(GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, 9, 54, 99), 9);
+            Assert.IsTrue(TestGetGcdFromIntegers_TimeSpend(timeSpend));            
+        }
+
+        [TestMethod]
+        public void GetGcdTestFromIntegers_minus9_minus55()
+        {
+            Assert.AreEqual(GCD.GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, -5, -55), 5);
+            Assert.IsTrue(TestGetGcdFromIntegers_TimeSpend(timeSpend));            
+        }
+
+        public bool TestGetGcdFromIntegers_TimeSpend(Stopwatch _timeSpend)
+        {
+            int timeCheck = (int)_timeSpend.ElapsedTicks;
+            if (timeCheck < 140000)
+            {
+                return true;
+            }
+
+            Assert.Fail($"timeSpend = {(double)timeCheck/100000}ms");
+            return false;
+        }
+    }
+}
---
+++ NET.W.2019.Zakharchuk.04/GCD.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FindGCD
+{
+    public class GCD
+    {
+        /// <summary>
+        /// This method calculates GCD using the Euclidean algorithm for random count of numbers.
+        /// </summary>
+        /// <param name="timeSpend">Time for which the method is executed</param>
+        /// <param name="array">Array of integers</param>
+        /// <exception cref="ArgumentNullException">If array equals null</exception>
+        /// <returnsNumber> GCD</returns>
+        public static int GetGcdByEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, params int[] array)
+        {
+            timeSpend = new Stopwatch();
+            timeSpend.Start();
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            int numberGcd = GetGcdByEuclideanAlgorithmFromIntegers(array);
+            timeSpend.Stop();            
+            return numberGcd;
+        }
+
+        /// <summary>
+        /// This method calculates GCD using the Euclidean algorithm for random count of numbers.
+        /// </summary>        
+        /// <param name="array">Array of integers</param>
+        /// <exception cref="ArgumentOutOfRangeException">If number of array more int.MaxValue or less int.MinValue</exception>
+        /// <returnsNumber> GCD</returns>
+        public static int GetGcdByEuclideanAlgorithmFromIntegers(params int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] <= int.MinValue || array[i + 1] <= int.MinValue || array[i] >= int.MaxValue || array[i + 1] >= int.MaxValue)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                if (array[i] == 0)
+                {
+                    array[i] = array[i + 1];
+                    continue;
+                }
+
+                if (array[i + 1] == 0)
+                {
+                    array[i + 1] = array[i];
+                    continue;
+                }
+
+                if (array[i] <= -1)
+                {
+                    array[i] = Math.Abs(array[i]);
+                }
+
+                if (array[i + 1] <= -1)
+                {
+                    array[i + 1] = Math.Abs(array[i + 1]);
+                }
+
+                while (array[i] != array[i + 1])
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        array[i] -= array[i + 1] + 1;
+                    }
+                    else
+                    {
+                        array[i + 1] -= array[i];
+                    }
+                }
+            }
+
+            return array[array.Length - 1];
+        }
+
+        /// <summary>
+        /// This method calculates GCD using the binary Euclidean algorithm for random count of numbers.
+        /// </summary>
+        /// <param name="timeSpend">Time for which the method is executed</param>
+        /// <param name="array">Array of integers</param>
+        /// <exception cref="ArgumentNullException">If array equals null</exception>
+        /// <returnsNumber GCD</returns>
+        public static int GetGcdByBinaryEuclideanAlgorithmFromIntegers(out Stopwatch timeSpend, params int[] array)
+        {
+            timeSpend = new Stopwatch();
+            timeSpend.Start();
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            int numberGcd = GetGcdByBinaryEuclideanAlgorithmFromIntegers(array);
+            timeSpend.Stop();
+            return numberGcd;
+        }
+
+        /// <summary>
+        /// This method calculates GCD using the binary Euclidean algorithm for random count of numbers.
+        /// </summary>        
+        /// <param name="array">Array of integers</param>
+        /// <exception cref="ArgumentOutOfRangeException">If number of array more int.MaxValue or less int.MinValue</exception>
+        /// <returnsNumber> GCD</returns>
+        public static int GetGcdByBinaryEuclideanAlgorithmFromIntegers(params int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] == int.MinValue || array[i + 1] == int.MinValue)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                if (array[i] == array[i + 1])
+                {
+                    return array[i];
+                }
+
+                if (array[i] == array[i + 1])
+                {
+                    array[i + 1] = array[i];
+                }
+
+                if (array[i] == 0)
+                {
+                    array[i] = array[i + 1];
+                    continue;
+                }
+
+                if (array[i + 1] == 0)
+                {
+                    array[i + 1] = array[i];
+                    continue;
+                }
+
+                if (array[i] <= -1)
+                {
+                    array[i] = ~array[i] + 1;
+                }
+
+                if (array[i + 1] <= -1)
+                {
+                    array[i + 1] = ~array[i + 1] + 1;
+                }
+
+                if (array[i] > 0 && array[i + 1] > 0)
+                {
+                    if ((array[i] & 1) != 1)
+                    {
+                        if ((array[i + 1] & 1) != 1)
+                        {
+                            return GetGcdByBinaryEuclideanAlgorithmFromIntegers(array[i] >> 1, array[i + 1] >> 1) << 1;
+                        }
+                        else
+                        {
+                            return GetGcdByBinaryEuclideanAlgorithmFromIntegers(array[i] >> 1, array[i + 1]);
+                        }
+                    }
+
+                    if ((array[i] & 1) == 1)
+                    {
+                        if ((array[i + 1] & 1) != 1)
+                        {
+                            return GetGcdByBinaryEuclideanAlgorithmFromIntegers(array[i], array[i + 1] >> 1);
+                        }
+
+                        if ((array[i + 1] & 1) == 1 && array[i + 1] > array[i])
+                        {
+                            return GetGcdByBinaryEuclideanAlgorithmFromIntegers((array[i + 1] - array[i]) >> 1, array[i]);
+                        }
+
+                        if ((array[i + 1] & 1) == 1 && array[i + 1] < array[i])
+                        {
+                            return GetGcdByBinaryEuclideanAlgorithmFromIntegers((array[i] - array[i + 1]) >> 1, array[i + 1]);
+                        }
+                    }
+                }
+            }
+
+            return array[array.Length - 1];
+        }
+    }
+}
